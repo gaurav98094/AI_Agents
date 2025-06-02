40257d8c6581b6ef2e8fda26402e561186123dc7
G <g@Gs-MacBook-Pro.local>
2025-06-02 23:42:54 +0530

    AI Agents - DDS

---
+++ book_writing_flow/src/book_writing_flow/crews/writer_crew/writer_crew.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool
+
+
+class Chapter(BaseModel):
+    """Chapter of the book"""
+
+    title: str
+    content: str
+
+
+@CrewBase
+class ChapterWriterCrew:
+
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def topic_researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["topic_researcher"], tools=[SerperDevTool()]
+        )
+
+    @task
+    def research_topic(self) -> Task:
+        return Task(config=self.tasks_config["research_topic"])
+
+    @agent
+    def writer(self) -> Agent:
+        return Agent(config=self.agents_config["writer"])
+
+    @task
+    def write_chapter(self) -> Task:
+        return Task(config=self.tasks_config["write_chapter"], output_pydantic=Chapter)
+
+    @crew
+    def crew(self) -> Crew:
+
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
